3c2a15bfa7e3c707a416fd4e3c0cae8dd41203f4
Wolfgang Schreurs <wolfgang@wolftrail.net>
2019-05-02 01:06:23 +0700

    Add Fotoapparat project, fix several issues regarding Fotoapparat

--- Demo/MainActivity.cs
+++ Demo/MainActivity.cs
@@ -6,13 +6,17 @@ using Android.Support.Design.Widget;
 using Android.Support.V7.App;
 using Android.Views;
 using Android.Widget;
-using Com.Peir.Photector;
+//using Com.Peir.Photector;
+using IO.Fotoapparat;
+using Java.Lang;
 
 namespace Demo
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        Fotoapparat camera;
+        IO.Fotoapparat.View.CameraView cameraView;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -23,8 +27,27 @@ namespace Demo
             Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
             SetSupportActionBar(toolbar);
 
+            this.cameraView = (IO.Fotoapparat.View.CameraView)FindViewById(Resource.Id.cameraView1);
+
             FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
+
+            this.camera = Fotoapparat.With(this).Into(cameraView).Build();
+
             fab.Click += FabOnClick;
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+
+            camera.Start();
+        }
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+
+            camera.Stop();
         }
 
         public override bool OnCreateOptionsMenu(IMenu menu)
@@ -49,12 +72,26 @@ namespace Demo
             View view = (View)sender;
             Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
                 .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
+                
+
+            var photoResult = camera.TakePicture();
+            photoResult.ToBitmap().WhenDone(new Test());
         }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    class Test : Java.Lang.Object, IO.Fotoapparat.Result.IWhenDoneListener
+    {
+
+        public void WhenDone(Java.Lang.Object p0)
+        {
+            throw new NotImplementedException();
         }
     }
 }
